b07bc3c78d8aa500501a36bee33bd14ed6442f9d
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2024-12-05 11:02:54 +0530

    final push

--- blog_server/blog/views.py
+++ blog_server/blog/views.py
@@ -6,6 +6,12 @@ from django.urls import reverse
 # A simple view to render the index page
 def index(request):
     blog_title = "Dynamic new title"
+    posts = [
+        {'id': 1, 'title': 'Post 1', 'content': 'This is the first post'},
+        {'id': 2,'title': 'Post 2', 'content': 'This is the second post'},
+        {'id': 3,'title': 'Post 3', 'content': 'This is the third post'},   
+        {'id': 4,'title': 'Post 4', 'content': 'This is the fourth post'},
+    ]
     return render(request, "index.html", {"blog_title": blog_title})
 
 # Optionally, you can create more views for different pages or actions in your app. 
